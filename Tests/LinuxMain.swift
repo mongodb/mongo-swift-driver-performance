9e28ac69858e48bd72586341e147c3c41e068476
kmahar <kaitlinmahar@gmail.com>
2018-11-30 14:18:22 -0500

    call MongoSwift.initialize() in base test class

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,11 +1,5 @@
 import Benchmarks
-import MongoSwift
 import XCTest
-
-/* Ensure libmongoc is initialized. Since XCTMain never returns, we have no
- * opportunity to cleanup libmongoc (either explicitly or with a deinit). This
- * may appear as a memory leak. */
-MongoSwift.initialize()
 
 var tests = [XCTestCaseEntry]()
 tests += Benchmarks.allTests()
